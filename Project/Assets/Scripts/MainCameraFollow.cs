b0a2c473172770017246ea421f31fc71708f25f8
Ben Powley <ben.powley97@gmail.com>
2014-04-28 21:46:41 +0100

    Working on movement

---
+++ Project/Assets/Scripts/MainCameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraFollow : MonoBehaviour 
+{
+	public GameObject player;
+	Vector3 camPos;
+	Vector3 playerPos;
+
+	public void Update()
+	{
+		this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+	}
+}
---
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed = 5f;
+	public float lerpVal = 0.2f;
+
+	public void FixedUpdate() 
+	{
+		rigidbody2D.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * speed, lerpVal), Mathf.Lerp(0, Input.GetAxis("Vertical") * speed, lerpVal));
+	}
+}
