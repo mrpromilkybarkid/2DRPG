aeb7232199ef30f8636207584c41a8412b227695
Ben Powley <ben.powley97@gmail.com>
2014-04-28 21:54:45 +0100

    SmoothCam

---
+++ Project/Assets/Scripts/GlobalPlayerSettings.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalPlayerSettings : MonoBehaviour 
+{
+	public static bool smoothCam = false;
+	public bool active = false;
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			if (active)
+			{
+				active = false;
+			}
+			else
+			{
+				active = true;
+			}
+		}
+	}
+
+	public void OnGUI()
+	{
+		if (active)
+		{
+			if (GUI.Button(new Rect(100, 100, 100, 100), "SC"))
+			{
+				if (smoothCam)
+				{
+					smoothCam = false;
+				}
+				else if (!smoothCam)
+				{
+					smoothCam = true;
+				}
+			}
+		}
+		else
+		{
+			//
+		}
+	}
+}
--- Project/Assets/Scripts/MainCameraFollow.cs
+++ Project/Assets/Scripts/MainCameraFollow.cs
@@ -9,6 +9,16 @@ public class MainCameraFollow : MonoBehaviour
 
 	public void Update()
 	{
-		this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+		camPos = new Vector3(this.transform.position.x, this.transform.position.y, -10);
+		playerPos = new Vector3(player.transform.position.x, player.transform.position.y, -10);	
+
+		if (GlobalPlayerSettings.smoothCam)
+		{
+			this.transform.position = Vector3.Lerp(camPos, playerPos, Time.deltaTime);
+		}
+		else
+		{
+			this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+		}
 	}
 }
