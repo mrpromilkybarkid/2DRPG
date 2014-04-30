4c28978ad7004acc893373dbffa25bd144c79905
Ben Powley <ben.powley97@gmail.com>
2014-04-30 09:02:24 +0100

    Enterable Houses...Sort of

--- Project/Assets/Scripts/PlayerController.cs
+++ Project/Assets/Scripts/PlayerController.cs
@@ -4,4 +4,48 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	public float playerHealth = 100f;
+	public float playerMana = 100f;
+
+	private bool playerDead = false;
+	private bool inEnterTrigger = false;
+
+	public void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public void Start()
+	{}
+
+	public void Update()
+	{
+		if (playerHealth <= 0)
+		{
+			playerDead = true;
+		}
+		else
+		{
+			playerDead = false;
+		}
+
+		if (inEnterTrigger)
+		{
+			if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				Debug.Log("Entered");
+			}
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.transform.tag == "Enterable")
+		{
+			inEnterTrigger = true;
+		}
+		else
+		{
+			inEnterTrigger = false;
+		}
+	}
 }
